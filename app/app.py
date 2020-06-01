c59d696439b4739d5debaccc1875e9df6d00dc6a
Gautam <30121546+gautam-b@users.noreply.github.com>
2020-06-01 19:55:33 +0530

    added telegram path

--- app/app.py
+++ app/app.py
@@ -6,5 +6,10 @@ def hello_world():
     return 'Hello, from the world of docker!'
 
 
+@app.route('/telegram/<context>')
+def telegram(context=None):
+    return f"Hello, {context}"
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port='7000')
